4d67160b6873ef174e64c26d5fd48754ff56d4a6
Serhii Soloviov <solovsrg@gmail.com>
2019-07-16 01:54:19 +0300

    ii

--- src/main/java/elma/streetsandpits/controller/Contr.java
+++ src/main/java/elma/streetsandpits/controller/Contr.java
@@ -5,35 +5,21 @@ import elma.streetsandpits.model.Pit;
 import elma.streetsandpits.model.Street;
 import elma.streetsandpits.service.StreetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class Contr  {
+public class Contr {
 
     @Autowired
-    StreetService streetService;
+    private StreetService streetService;
 
-    @RequestMapping("/add")
-    public Response uploadMultipleFiles(@RequestParam(value="name") String name) {
-        Street street = new Street();
-        street.setName(name);
-
-        Pit pit = new Pit();
-        pit.setStreet(street);
-        List<Pit>pits = street.getPitList();
-        pits.add(pit);
-        street.setPitList(pits);
-        streetService.saveStreet(street);
-
-        Response response = new Response();
-        response.setId(street.getId());
-        response.setName(street.getName());
-
-        return response;
+    @PostMapping("/streets")
+    public void addStreet(@RequestBody Street street) {
+        Street newStreet = new Street();
+        street.setName(street.getName());
+        streetService.saveStreet(newStreet);
     }
 
 }
